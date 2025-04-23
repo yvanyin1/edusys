e82e522cd72eac28aa684fab828c71bb2065657e
Daniel Luo <danbrosluo2011@gmail.com>
2025-04-23 16:42:45 -0400

    Integrated all UI and page interactions for Create Course Profile

--- app/app.py
+++ app/app.py
@@ -18,5 +18,20 @@ def create_course_profile():
     return render_template("create_course_profile_form.html", username="dluo")
 
 
+@app.route('/course-created', methods=['POST'])
+def course_profile_created():
+    course_data = {
+        'course_name': request.form['course_name'],
+        'course_code': request.form['course_code'],
+        'course_desc': request.form['course_desc'],
+        'target_audience': request.form['target_audience'],
+        'duration_in_weeks': request.form['duration_in_weeks'],
+        'prerequisites': request.form['prerequisites'],
+        'corequisites': request.form['corequisites'],
+        'credit_hours': request.form['credit_hours']
+    }
+    return render_template("create_course_profile_success.html", course=course_data)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
